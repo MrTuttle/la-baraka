fecd2cafd8fab45a961a2768b3a454333e367cb6
Florent <florent.vincerot@me.com>
2023-11-05 22:30:31 +0100

    enhance cloudinary upload widget

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+"use client";
 // app/issue/page.tsx
 
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import { CldImage } from "next-cloudinary";
 
 const home = () => {
-  return <Button>New Issue</Button>;
+  return (
+    <>
+      <Button>New Issue</Button>
+      <CldImage
+        width="960"
+        height="600"
+        src="kh4tt0exbzev7p3csh32"
+        sizes="100vw"
+        alt="Description of my image"
+      />
+    </>
+  );
 };
 
 export default home;
