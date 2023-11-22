3dbe0446860ef0b257c13df7845c307b09635395
Florent <florent.vincerot@me.com>
2023-11-22 11:38:24 +0100

    protecting admin route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,9 +9,13 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 const pageDashboard = async () => {
   const session = await getServerSession(authOptions);
   return (
-    <div className="mx-4">
+    <div className="prose mx-4">
       <h1>Dashboard</h1>
       <p>Bonjour {session && <span>{session.user!.name}</span>}</p>
+      <Flex gap="5">
+        <Link href="api/auth/signin">Sign in</Link>
+        <Link href="api/auth/signout">Signout</Link>
+      </Flex>
       <Flex gap="3" direction="column">
         <Box>
           <Link href="/chambres/new">Nouvelle chambre</Link>
