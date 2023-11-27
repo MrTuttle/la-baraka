a93cb32b4d7821ea6fb29ec9c1c011c85ebc48ca
Florent <florent.vincerot@me.com>
2023-11-27 16:35:41 +0100

    style modification and deletion links for chambres

--- app/chambres/page.tsx
+++ app/chambres/page.tsx
@@ -2,9 +2,11 @@ import prisma from "@/prisma/client";
 import React from "react";
 import { number } from "zod";
 import ReactMarkdown from "react-markdown";
-import { Button, Card } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Text } from "@radix-ui/themes";
 import DisplayCld from "../components/DisplayCld";
 import Link from "next/link";
+import { HiOutlineTrash } from "react-icons/hi2";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
 
 interface Chambre {
   title: string;
@@ -24,7 +26,29 @@ const chambresPage = async () => {
         </Link>
         {chambres.map((chambre) => (
           <Card key={chambre.id} className="prose" mt="4">
-            <h3>{chambre.title}</h3>
+            <Flex direction="row" justify="between" align="baseline" gap="0">
+              <Box>
+                <h3>{chambre.title}</h3>
+              </Box>
+              <Flex gap="2" direction="column">
+                <Link href={"/chambres/new"}>
+                  <Flex gap="1">
+                    <HiOutlinePencilSquare />
+                    <Text as="p" size="1">
+                      Modifier
+                    </Text>
+                  </Flex>
+                </Link>
+                <Link href="/chambres/new">
+                  <Flex gap="1">
+                    <HiOutlineTrash />
+                    <Text as="p" size="1">
+                      Supprimer
+                    </Text>
+                  </Flex>
+                </Link>
+              </Flex>
+            </Flex>
             <ReactMarkdown>{chambre.description}</ReactMarkdown>
           </Card>
         ))}
