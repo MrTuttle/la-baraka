cdca40680528f715cf55a43fab8c349f1713ae21
Florent <florent.vincerot@me.com>
2023-12-07 21:26:45 +0100

    booked days config - not work yet

--- app/components/datePicker/BKDayPicker.tsx
+++ app/components/datePicker/BKDayPicker.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { format } from "date-fns";
+import { fr } from "date-fns/locale";
 import { useState } from "react";
-import { DateRange, DayPicker } from "react-day-picker";
+import { DateRange, DayPicker, DayClickEventHandler } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 
-const pastMonth = new Date(2020, 10, 15);
+const bookedDays = [new Date(2023, 12, 20), new Date(2023, 12, 23)];
+const bookedStyle = { border: "2px solid currentColor" };
 
 const BKDayPicker = () => {
   const [range, setRange] = useState<DateRange | undefined>();
+  const [booked, setBooked] = useState(false);
+
+  const handleDayClick: DayClickEventHandler = (day, modifiers) => {
+    setBooked(day && modifiers.booked);
+  };
 
   let footer = <p>Please pick the first day.</p>;
   if (range?.from) {
@@ -25,11 +32,18 @@ const BKDayPicker = () => {
   return (
     <DayPicker
       id="test"
-      mode="range"
-      defaultMonth={pastMonth}
+      mode="range" // ability to select a range of days
       selected={range}
       onSelect={setRange}
       footer={footer}
+      locale={fr} // locale on france
+      fromYear={2023} // limits years
+      toYear={2025}
+      showOutsideDays
+      onDayClick={handleDayClick}
+      modifiers={{ booked: bookedDays }}
+      modifiersStyles={{ booked: bookedStyle }}
+      // captionLayout="dropdown"
     />
   );
 };
