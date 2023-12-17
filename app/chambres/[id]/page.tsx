e319b7ab34047babdbb553da537f692491e4e5e1
Florent <florent.vincerot@me.com>
2023-12-17 22:24:18 +0100

    styles

--- app/chambres/[id]/page.tsx
+++ app/chambres/[id]/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import DeleteRoomButton from "./DeleteRoomButton";
 import GetCldIdList from "@/app/components/GetCldIdList";
 import BKDayPicker from "@/app/components/datePicker/BKDayPicker";
-import { Button, Flex } from "@radix-ui/themes";
+import { Box, Button, Flex } from "@radix-ui/themes";
 
 interface Props {
   // params id: typed in string, 'cause url are always string
@@ -38,15 +38,26 @@ const ChambreDetailPage = async ({ params }: Props) => {
       <Flex direction="column" align="center" className="p-8">
         <BKDayPicker />
       </Flex>
-
-      <DeleteRoomButton roomId={room.id} />
+      <div className="pt-10 pb-32">
+        <DeleteRoomButton roomId={room.id} />
+      </div>
       <div
         className="w-full bg-white h-20 border-t-2
             fixed left-0 bottom-0
-            flex justify-center items-center z-50
+            flex justify-between items-center z-50
            "
       >
-        <Button>Reserver</Button>
+        <div className="p-4">
+          <p>
+            <strong>{room.id} € </strong>par nuits
+          </p>
+          <p>8-9 jan</p>
+        </div>
+        <div className="p-4">
+          <button className=" px-24 bg-red-500 hover:bg-red-600 transition-all p-4 rounded-md text-white">
+            Réserver
+          </button>
+        </div>
       </div>
     </Flex>
   );
