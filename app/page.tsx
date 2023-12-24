7ef1056e01884b0583bd674ac9b3324e2e7d8a24
Florent <florent.vincerot@me.com>
2023-12-24 16:48:23 +0100

    add home image

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,19 @@ import MenuSwiper from "./components/swiper/MenuSwiper";
 import CallMenuSwiper from "./components/swiper/CallMenuSwiper";
 import UploadWidget from "./components/UploadWidget";
 import SlidePerViewGetIds from "./components/swiper/SlidePerViewGetIds";
+import CldImageClient from "./components/CldImageClient";
 
 const home = async () => {
   // const chambresImage = await prisma.image.findMany();
   return (
     <>
+      <CldImageClient
+        src="r6sztwmaweq1sehluh74"
+        width={600}
+        height={600}
+        alt="baraka"
+        sizes="100vw"
+      />
       <Section className="prose m-4">
         <h1>La Baraka vous accueille au cœur des monts des Cevennes</h1>
         <p>
@@ -26,6 +34,7 @@ const home = async () => {
           dolores aliquam. Laborum!
         </p>
       </Section>
+
       <Section className="prose">
         <h2 className="m-4">Au menu aujourd’hui</h2>
         <CallMenuSwiper />
