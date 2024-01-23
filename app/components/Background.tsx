e520e87b2029791a2b8cae05112b915f0785fd5e
Florent <florent.vincerot@me.com>
2024-01-23 19:23:28 +0100

    scrolltrigger experimentations

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import React from "react";
 import terrasse from "@/public/upload/la-baraka-terrasse.jpg";
 
-export default function Background() {
+export default function Background(ref?: React.MutableRefObject<null>) {
   return (
     <Image
+      ref={ref}
       alt="Terrasse"
       src={terrasse}
       placeholder="blur"
--- app/components/HeroScrollEffect.tsx
+++ app/components/HeroScrollEffect.tsx
@@ -12,140 +12,203 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger);
 
 const HeroScrollEffect = () => {
-  const headerRef = useRef(null);
+  const logoRef = useRef(null);
   const bgRef = useRef(null);
   const bgRefContainer = useRef(null);
 
-  const main = useRef(null);
+  // define main who contain several <HTMLElement>(".box")
+  // const main = useRef(null);
 
-  useGSAP(
-    () => {
-      const boxes = gsap.utils.toArray<HTMLElement>(".box");
-      boxes.forEach((box) => {
-        gsap.to(box, {
-          y: 150,
-          scrollTrigger: {
-            trigger: box,
-            start: "bottom bottom",
-            end: "top 20%",
-            scrub: true,
-            markers: true,
-          },
-        });
-      });
-    },
-    { scope: main }
-  );
+  // scrollTrigger on main :
+  // useGSAP(
+  //   () => {
+  // define an array of <HTMLElement>(".box")
+  // const boxes = gsap.utils.toArray<HTMLElement>(".box");
+  // target all <HTMLElement>(".box")
+  //   boxes.forEach((box) => {
+  //     gsap.to(box, {
+  //       y: 150,
+  //       scrollTrigger: {
+  //         trigger: box,
+  //         start: "bottom bottom",
+  //         end: "top 20%",
+  //         scrub: true,
+  //         markers: true,
+  //       },
+  //     });
+  //   });
+  // },
+  // { scope: main }
+  // {scope: main } applied only on all <HTMLElement>(".box")
+  // which contains in main ref, replace the array[] of dependencies of useEffect
+  // );
+  //----------- trigger on multi elements -----//
 
   useGSAP(() => {
-    gsap.from(headerRef.current, {
+    const descaleDisolveOnScrool = logoRef.current;
+    const containerMoveOnScrool = bgRefContainer.current;
+    console.log("bgImage", descaleDisolveOnScrool);
+
+    gsap.to(containerMoveOnScrool, {
+      onEnter: () => {
+        gsap.set(containerMoveOnScrool, { height: "100vh" });
+      },
+      scrollTrigger: {
+        trigger: containerMoveOnScrool,
+        start: "top top",
+        end: "bottom 15%",
+        scrub: true,
+        markers: true,
+      },
+      height: "10vh",
+    });
+
+    // descale & disolve on scroll -> hero logo
+    gsap.to(descaleDisolveOnScrool, {
+      scale: 1.1,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: descaleDisolveOnScrool,
+        start: "top 35%",
+        end: "bottom 25%",
+        scrub: true,
+
+        // markers: true,
+        onEnter: () => {
+          // set imediate properties
+          // gsap.set(trigerredItem, { position: "fixed" });
+          // launch a tween
+          // gsap.to(trigerredItem, { y: -40 });
+        },
+        onLeave: () => {
+          // gsap.set(trigerredItem, { rotate: 90 });
+        },
+      },
+    });
+    gsap.from(bgRef.current, {
       duration: 3,
       autoAlpha: 0.1,
-      delay: 3,
+      // delay: 3,
       ease: "back.out(1.7)",
       y: 10,
     });
   });
 
-  useGSAP(() => {
-    gsap.to(bgRef.current, {
-      duration: 3,
-      delay: 1,
-      ease: "back.out(1.7)",
+  // useGSAP(() => {
+  //   gsap.from(logoRef.current, {
+  //     duration: 3,
+  //     autoAlpha: 0.1,
+  //     // delay: 3,
+  //     ease: "back.out(1.7)",
+  //     y: 10,
+  //   });
+  // });
 
-      scale: 1.1,
-      rotate: -25,
-    });
-    console.log("BGREF:", bgRef.current);
-  });
-  useGSAP(() => {
-    gsap.to(bgRefContainer.current, {
-      duration: 3,
-      delay: 1,
-      ease: "back.out(1.7)",
+  // useGSAP(() => {
+  //   gsap.to(bgRef.current, {
+  //     duration: 3,
+  //     delay: 1,
+  //     ease: "back.out(1.7)",
 
-      scale: 2,
-      rotate: 25,
-    });
-    console.log("BGREFCONTAINER:", bgRefContainer.current);
-  });
-  useEffect(() => {
-    // const element = headerRef.current;
-    // console.log("HEADERREF:", headerRef);
-    //------------//
-    // gsap.from(headerRef.current, {
-    //   duration: 1,
-    //   autoAlpha: 0.5,
-    //   delay: 1,
-    //   y: 100,
-    // });
-    //------------//
+  //     scale: 1.1,
+  //     rotate: -25,
+  //   });
+  //   console.log("BGREF:", bgRef.current);
+  // });
 
-    // ScrollTrigger.create({
-    //   trigger: ref.current,
-    //   start: "-0.1% top",
-    //   end: "bottom bottom",
-    //   onEnter: () => {
-    //     gsap.set(ref.current, { position: "absolute", top: "195%" });
-    //   },
-    //   onLeave: () => {
-    //     gsap.set(ref.current, { position: "relative", top: "0" });
-    //   },
-    //   scrub: true,
-    //   markers: false,
-    // });
-    //----------------//
-    // gsap.to(element, {
-    //   scrollTrigger: {
-    //     trigger: element,
-    //     start: "-0.1% top",
-    //     end: "bottom bottom",
-    //     onEnter: () => {
-    //       gsap.set(headerRef.current, {
-    //         position: "absolute",
-    //         top: "195%",
-    //         rotate: "125",
-    //       });
-    //     },
-    //     onLeave: () => {
-    //       gsap.set(element, {
-    //         position: "relative",
-    //         top: "0",
-    //         rotate: "2",
-    //       });
-    //     },
-    //     scrub: true,
-    //     markers: false,
-    //   },
-    //   opacity: 0.2,
-    //   rotation: 1,
-    // });
-    //-------//
-    return () => {
-      // element.revert();
-    };
-  }, []);
+  //---- rotate & scale ----//
+  // useGSAP(() => {
+  //   gsap.to(bgRefContainer.current, {
+  //     duration: 3,
+  //     delay: 1,
+  //     ease: "back.out(1.7)",
+
+  //     scale: 2,
+  //     rotate: 25,
+  //   });
+  //   console.log("BGREFCONTAINER:", bgRefContainer.current);
+  // });
+  //------ //
+
+  // useEffect(() => {
+  // const element = logoRef.current;
+  // console.log("logoRef:", logoRef);
+  //------------//
+  // gsap.from(logoRef.current, {
+  //   duration: 1,
+  //   autoAlpha: 0.5,
+  //   delay: 1,
+  //   y: 100,
+  // });
+  //------------//
+
+  // ScrollTrigger.create({
+  //   trigger: ref.current,
+  //   start: "-0.1% top",
+  //   end: "bottom bottom",
+  //   onEnter: () => {
+  //     gsap.set(ref.current, { position: "absolute", top: "195%" });
+  //   },
+  //   onLeave: () => {
+  //     gsap.set(ref.current, { position: "relative", top: "0" });
+  //   },
+  //   scrub: true,
+  //   markers: false,
+  // });
+  //----------------//
+  // gsap.to(element, {
+  //   scrollTrigger: {
+  //     trigger: element,
+  //     start: "-0.1% top",
+  //     end: "bottom bottom",
+  //     onEnter: () => {
+  //       gsap.set(logoRef.current, {
+  //         position: "absolute",
+  //         top: "195%",
+  //         rotate: "125",
+  //       });
+  //     },
+  //     onLeave: () => {
+  //       gsap.set(element, {
+  //         position: "relative",
+  //         top: "0",
+  //         rotate: "2",
+  //       });
+  //     },
+  //     scrub: true,
+  //     markers: false,
+  //   },
+  //   opacity: 0.2,
+  //   rotation: 1,
+  // });
+  //-------//
+  // return () => {
+  // element.revert();
+  // };
+  // }, []);
 
   return (
     <>
-      <div ref={main}>
+      <div>
         <div
-          ref={headerRef}
-          className="box prose fixed h-56 w-56 top-80 left-[16%] md:top-1/2 md:left-1/2 md:-translate-x-2/4 md:-translate-y-2/4 text-white  border-[1px]  z-[2]"
+          ref={logoRef}
+          className="box prose fixed h-56 w-56 top-80 left-[16%] md:top-1/2 md:left-1/2 md:-translate-x-2/4 md:-translate-y-2/4 text-gray-400  border-[1px] border-gray-400  z-[2]"
         >
           <div className="flex flex-col gap-4 items-center text-4xl px-20 pt-5">
             <SiForestry />
 
-            <h1 className="text-white font-extralight text-5xl">La Baraka</h1>
+            <h1 className=" text-gray-400 font-extralight text-5xl">
+              La Baraka
+            </h1>
           </div>
         </div>
         {/* </div> */}
         <div>
           <div
             ref={bgRefContainer}
-            className="box App-header relative top-0 w-full h-screen mask-clip"
+            className="box App-header relative top-0 w-full h-[75%] my-border-red"
           >
-            {/* <Background /> */}
+            {/* <Background ref={bgRef} /> */}
             <Image
               ref={bgRef}
               alt="Terrasse"
@@ -155,6 +218,7 @@ const HeroScrollEffect = () => {
               fill
               sizes="110vw"
               style={{ objectFit: "cover" }}
+              // style={{ height: "150px" }}
               className="box"
             />
           </div>
