dbf3f1efd002fae431bb96071e34f90914130235
Florent <florent.vincerot@me.com>
2024-01-25 12:04:34 +0100

    img home & intro text

--- app/page.tsx
+++ app/page.tsx
@@ -42,39 +42,32 @@ const home = () => {
       <HeroScrollEffect />
       <div>
         <div className="content-holder">
-          <Section className=" px-8 h-screen flex flex-col items-center w-full mx-auto my-auto bg-white align-middle">
-            {/* <SplitTypeFramer> */}
-            <h3 className=" prose font-light text-2xl md:text-[2.5rem] self-center leading-[3rem] md:leading-[4rem] mx-3">
-              La Baraka vous accueille au cœur du massif des Cevennes. Lorem
-              ipsum dolor sit, amet consectetur <strong>adipisicing</strong>{" "}
-              elit. Laboriosam, est? Quae vel adipisci provident tempore, nisi,
-              commodi suscipit pariatur perspiciatis beatae modi nam, iste aut
-              possimus consequatur dolores aliquam. Laborum!
-            </h3>
-
-            {/* </SplitTypeFramer> */}
+          <Section className=" justify-center px-11 h-screen flex flex-col items-center w-full mx-auto my-auto bg-white align-middle">
+            <SplitTypeFramer>
+              <h3 className=" prose font-light text-2xl md:text-[2.5rem] self-center leading-[3rem] md:leading-[4rem]">
+                La Baraka vous accueille au cœur du massif des Cevennes. Lorem
+                ipsum dolor sit, amet consectetur <strong>adipisicing</strong>{" "}
+                elit. Laboriosam, est? Quae vel adipisci provident tempore,
+                nisi, commodi suscipit pariatur perspiciatis beatae modi nam,
+                iste aut possimus consequatur dolores aliquam. Laborum!
+              </h3>
+            </SplitTypeFramer>
+            <div className="mt-11">
+              <SectionFramer>
+                <Image
+                  src={backgroundImage}
+                  alt="portrait"
+                  // fill
+                  className="object-cover"
+                  sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+                  // 480px = standart mobiles, 768 = standard tablets, 33vw other width devices -> seem not working on dev tools, see in real mobile device
+                  quality="75" // default = 75 percent
+                  // priority // boolean, set priority to images above the fold
+                />
+              </SectionFramer>
+            </div>
           </Section>
-          <div
-            style={{
-              // use the src property of the image object
-              backgroundImage: `url(${backgroundImage.src})`,
-              // other styles
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              width: "100vw",
-              height: "400px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundAttachment: "fixed",
-              marginTop: "10rem",
-            }}
-          >
-            {/* <h1 style={{ fontSize: 50, color: "white" }}>
-                Welcome to Sling Academy!
-              </h1> */}
-          </div>
+
           <section className="prose min-h-screen py-10 mx-auto bg-gray-100">
             <SectionFramer>
               <h2 className="px-8 font-normal text-5xl">
