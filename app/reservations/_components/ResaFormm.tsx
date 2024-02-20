1c29fc694b283962e25acef1b3428a89118d29e1
Florent <florent.vincerot@me.com>
2024-02-20 02:11:23 +0100

    correction checkIn in resaFormm

--- app/reservations/_components/ResaFormm.tsx
+++ app/reservations/_components/ResaFormm.tsx
@@ -135,7 +135,7 @@ const ResaFormm = ({ resa }: { resa?: FormValues }) => {
                 Check Out
               </label>
               <input
-                defaultValue={resa?.checkIn.toISOString()}
+                defaultValue={resa?.checkOut.toISOString()}
                 {...register("checkOut", { valueAsDate: true })}
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               />
